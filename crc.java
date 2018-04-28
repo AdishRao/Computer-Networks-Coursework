1f6212155565ffc4eddb5f5a1d44002ab8d81b6b
Aniruddha Mysore <aniruddha.mysore@gmail.com>
2018-04-28 22:26:42 +0530

    convert 16bit bins to strs

--- crc.java
+++ crc.java
@@ -1,14 +1,29 @@
 import  java.util.*;
 
 public class crc {
-
+  private static String binToString(int bin) {
+    int i = 0;
+    String str = "";
+    for (i=0; i<16; i++) {
+      str += ( ( (bin<<i) & 0b1000000000000000) == 0 )?"0":"1";
+    }
+    return str;
+  } 
+ 
   public static void main(String args[]) {
-    String[] dbin = {"0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111",
-    "1000", "1001", "1001", "1010", "1011", "1100", "1101", "1110", "1111"};
-
-    String[] rbin = {"000", "001", "010", "011", "100", "101", "110", "111"};
-
     Scanner sc = new Scanner(System.in);
+
+    /* Test 
+        System.out.println("Binary("+0x0001+") [\"0x0001\"] = "+binToString(0x0001));
+        System.out.println("Binary("+0x0002+") [\"0x0002\"] = "+binToString(0x0002));
+        System.out.println("Binary("+0x000F+") [\"0x000F\"] = "+binToString(0x000F));
+        System.out.println("Binary("+0x00FF+") [\"0x00FF\"] = "+binToString(0x00FF));
+        System.out.println("Binary("+0x00F0+") [\"0x000F\"] = "+binToString(0x00F0));
+        System.out.println("Binary("+0x00F2+") [\"0x00F2\"] = "+binToString(0x00F2));
+        System.out.println("Binary("+0x02F2+") [\"0x02F2\"] = "+binToString(0x02F2));
+        System.out.println("Binary("+0xBEAD+") [\"0xBEAD\"] = "+binToString(0xBEAD));
+    */
+
     System.out.println("Enter dataword");
     int dataword = sc.nextInt();
 
@@ -28,6 +43,6 @@ public class crc {
 
       dataword = k << 1;
     }
-    System.out.println("finally : " + dbin[old]+  rbin[k]);
+    System.out.println("finally : " + binToString(0|(old<<r)|k));
   }
 }
