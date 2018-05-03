9e13d1f2555c244d34116c51a912f0617d8a2857
Aniruddha Mysore <aniruddha.mysore@gmail.com>
2018-05-03 19:57:33 +0530

    highest Power function

--- crc.java
+++ crc.java
@@ -9,40 +9,55 @@ public class crc {
     }
     return str;
   } 
+  private static String longToBinString(long bin) {
+    long i = 0;
+    String str = "";
+    for (i=0; i<32; i++) {
+      str += ( ( (bin<<i) & 0b10000000000000000000000000000000) == 0 )?"0":"1";
+    }
+    return str;
+  } 
  
+  static int highestPower(int bin) {
+    for(int i=0; i<16; i++) {
+      if( (bin<<i &  0b1000000000000000) != 0 ) 
+        return 15-i; 
+    }
+    return 0;
+  }
+
   public static void main(String args[]) {
     Scanner sc = new Scanner(System.in);
-
-    /* Test 
-        System.out.println("Binary("+0x0001+") [\"0x0001\"] = "+binToString(0x0001));
-        System.out.println("Binary("+0x0002+") [\"0x0002\"] = "+binToString(0x0002));
-        System.out.println("Binary("+0x000F+") [\"0x000F\"] = "+binToString(0x000F));
-        System.out.println("Binary("+0x00FF+") [\"0x00FF\"] = "+binToString(0x00FF));
-        System.out.println("Binary("+0x00F0+") [\"0x000F\"] = "+binToString(0x00F0));
-        System.out.println("Binary("+0x00F2+") [\"0x00F2\"] = "+binToString(0x00F2));
-        System.out.println("Binary("+0x02F2+") [\"0x02F2\"] = "+binToString(0x02F2));
-        System.out.println("Binary("+0xBEAD+") [\"0xBEAD\"] = "+binToString(0xBEAD));
-    */
-
     System.out.println("Enter dataword");
+    
     int dataword = sc.nextInt();
 
-    System.out.println("Enter highest power");
-    int r = sc.nextInt();
+    int r = highestPower(dataword);
 
-    int divisor = 11;
+    int divisor = 11, hpd = highestPower(divisor);
     int k = 0, old = dataword;
+    
+    for(int i=0; i<15; i++) {
 
-    for(int i=0; i<=r; i++) {
-      if(dataword>>3==1)
-        k = (dataword)^divisor;
-      else
-        k = dataword;
+      k = dataword >> (15-hpd);
 
-      System.out.println("iter " + i+ " : " + k);
+      if(k>>hpd==1) 
+        k = (k)^divisor;
 
-      dataword = k << 1;
+      dataword &= 0x0FFF;
+      
+      dataword |= k<<(15-hpd); 
+
+      dataword <<= 1;
+
+      System.out.println(binToString(dataword));
     }
-    System.out.println("finally : " + binToString(0|(old<<r)|k));
+    System.out.println("k is " + binToString(k));    
+    long n = (old<<hpd+1)|k;
+    String nstring = longToBinString(old);
+    
+    System.out.println("finally : " + nstring);
+    
+    sc.close();
   }
 }
