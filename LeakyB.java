e331e127bda267801faf817bc26b6b13b8655ace
Adish Rao <adish.rao98@gmail.com>
2018-10-30 21:57:53 +0530

    Hello

---
+++ LeakyB.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class LeakyB {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random r = new Random();
+        int opr, bs, dprpkt = 0, datainbs = 0, sent;
+        int[] ps = new int[15];
+        for (int i = 0; i < 15; i++) {
+            ps[i] = r.nextInt(8) + 1;
+        }
+        for (int i = 0; i < 15; i++) {
+            System.out.print(ps[i] + " ");
+        }
+        System.out.println("\nEnter Output Rate");
+        opr = sc.nextInt();
+        System.out.println("Enter Buffer Size");
+        bs = sc.nextInt();
+        int i = 0;
+        System.out.println("Time\tPkt Rcv\tPkt in Buff\tPkt Trans\tPkt Dropped");
+        while (i != 15) {
+            System.out.print(i + "\t");
+            System.out.print(ps[i] + "\t");
+            if ((ps[i] + datainbs) > bs) {
+                dprpkt = ps[i] - (bs - datainbs);
+                datainbs = bs;
+            } else {
+                datainbs += ps[i];
+                dprpkt = 0;
+            }
+            System.out.print(datainbs + "\t\t");
+            sent = (datainbs < opr) ? datainbs : opr;
+            datainbs = (datainbs < opr) ? 0 : datainbs - opr;
+            System.out.print(sent + "\t\t");
+            System.out.print(dprpkt + "\n");
+            i++;
+
+        }
+
+    }
+}
